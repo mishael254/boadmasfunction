c2b998e00dd604ae4e453aa991b64629e5dc2c95
mishael254 <mishaelmishantoz@gmail.com>
2022-08-26 12:44:42 +0300

    called the boadmas function to display the answer

--- main/main.cpp
+++ main/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 double bodmas(double a,double b, double c,double d, double e, double f) {
 	cout << "Enter any 6 numbers that you want to be worked on\n(remember to separate them using spaces to make it work)\n";
+
 	while (cin >> a >> b >> c >> d >> e >> f) {
 		return a * b / c * d + e - f;
 	}
