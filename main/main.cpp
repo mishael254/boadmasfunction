e4bc8a8c43c510544f4fe7dc9fe7e28b69798d34
mishael254 <mishaelmishantoz@gmail.com>
2022-08-26 12:39:44 +0300

    create a new function bodmas it takes in 6 interger parameters and then returns a boadmas equation

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <iostream>
 using namespace std;
-
 double bodmas(double a,double b, double c,double d, double e, double f) {
 	cout << "Enter any 6 numbers that you want to be worked on\n(remember to separate them using spaces to make it work)\n";
 	while (cin >> a >> b >> c >> d >> e >> f) {
